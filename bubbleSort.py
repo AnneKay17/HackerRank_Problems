e34e31af99362f206da9ed7d4f037983d62348b9
AnneKay17 <162749912+AnneKay17@users.noreply.github.com>
2026-02-24 22:10:23 +0200

    Add files via upload

---
+++ bubbleSort.py
@@ -0,0 +1,19 @@
+def countSwaps(a):
+    # Write your code here
+    size = len(a)
+    count = 0
+    for i in range(size):
+        for j in range(size -i -1):
+            if a[j] > a[j+1]:
+                temp = a[j]
+                a[j] = a[j+1]
+                a[j+1] = temp
+                count += 1
+    print(f"Array is sorted in {count} swaps.\nFirst Element: {a[0]}\nLast Element: {a[-1]}")
+        
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+    countSwaps(a)
